d6c3759a17ab6b1485e70467e0efe7170ff570e2
Frank Richter <frank.richter@gmail.com>
2015-03-29 21:00:14 +0200

    Use application font from theme

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <QApplication>
+#include <QFont>
 #include <QQmlApplicationEngine>
+#include <QScreen>
 
 #include "facade/RegisterTypes.hpp"
 #include "facade/Start.hpp"
 #include "piping/RegisterTypes.hpp"
 
+#include <Windows.h>
+#include <Uxtheme.h>
+#include <vssym32.h>
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    LOGFONTW lfw;
+    if (SUCCEEDED(GetThemeSysFont(NULL, TMT_MSGBOXFONT, &lfw)))
+    {
+      QFont font;
+      font.setFamily(QString::fromWCharArray(lfw.lfFaceName));
+      font.setPointSize((qAbs(lfw.lfHeight) * 72.0) / app.screens()[0]->logicalDotsPerInchY());
+      app.setFont(font);
+    }
 
     piping::RegisterTypes("SteamLibrarian", 1, 0);
     facade::RegisterTypes("SteamLibrarian", 1, 0);
